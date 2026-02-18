a2e2f12cb548ad00158438341e4dfa7a8784ae68
derick937 <derickmark827@gmail.com>
2026-02-18 12:12:21 +0100

    UserInput

---
+++ JavaOutput.java
@@ -0,0 +1,18 @@
+public class JavaOutput{
+
+public static void main(String[] args){
+	System.out.println("Hello world, this is my first Java Program");
+	System.out.println("Learning java is fun");
+	
+	
+	
+	//Using the print method
+	System.out.print("Do you Love \"Learning Java\"?");
+	System.out.print("Yes I love learning Java\n ");
+	
+	//Using the printf method
+	System.out.printf("My name is mercy Ben, i am %d years old %n",25);
+    }
+	
+}
+	
