53f7e300c5f02069c033b3fcb20f75c7e801c9c2
derick937 <derickmark827@gmail.com>
2026-02-23 09:29:08 +0100

    chapter 2

--- UserInput.java
+++ UserInput.java
@@ -13,12 +13,19 @@ public class UserInput{
 		System.out.print("Enter your age: ");
 		int age = scan.nextInt();
 		scan.nextLine();
+	
+		System.out.print("Enter your gender (M/F: ");
+		char gender = scan.next().charAt(0);
+		
+		System.out.printf("Are you happy to learn java?(true/false: ");
+		boolean isHappy = scan.nextBoolean();
 		
 		System.out.println("");
 		System.out.println("");
 		
 		System.out.printf("Hello %s , How are you today?",fullname);
 		System.out.printf("Nice meeting you , you are living in %s",address);
-		System.out.printf("%s, you are %d years old",fullName,age);
+		System.out.printf("%s, you are %d years old",fuLLName,age);
+		System.ouut.printf("You are ")
 	}
 }
